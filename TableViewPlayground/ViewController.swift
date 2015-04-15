054603228dc7fff4c7aa844c95bfd56046fe439f
paulou1009 <paulou1009@gmail.com>
2015-04-14 22:27:06 -0700

    Initial Commit

---
+++ TableViewPlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewPlayground
+//
+//  Created by Hanyang Ou on 4/14/15.
+//  Copyright (c) 2015 Paul Ou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
